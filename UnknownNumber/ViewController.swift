7a5d4857e3ab6f0c53719178ca3c1981884b051c
Pustahvar Aliaksandr <ap1285@mail.ru>
2023-07-25 15:40:34 +0300

    Initial Commit

---
+++ UnknownNumber/ViewController.swift
@@ -0,0 +1,218 @@
+//
+//  ViewController.swift
+//  UnknownNumber
+//
+//  Created by Aliaksandr Pustahvar on 24.07.23.
+//
+
+import UIKit
+import Combine
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    private let textField = UITextField()
+    private let resultLabel = UILabel()
+    private let titlelabel = UILabel()
+    private let timerLabel = UILabel()
+    private let textTimerLabel = UILabel()
+    private let restartButton = UIButton(type: .system)
+    private var randomNumber = 1
+    private var isCancel = false
+    private var timer: Timer?
+    
+    private var cancellables = Set<AnyCancellable>()
+    private var timerCancellables = Set<AnyCancellable>()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemIndigo
+        setupView()
+        textField.delegate = self
+        createRandomNumber()
+        setUpToolBar()
+        print(randomNumber)
+        createTextFieldSubscriber()
+        createTimerSubscriber()
+    }
+    
+    private func createTextFieldSubscriber() {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: textField)
+            .map { ($0.object as? UITextField)?.text }
+            .debounce(for: .seconds(0.8), scheduler: RunLoop.main)
+            .sink { string in
+                let value: Int = Int(string ?? "0") ?? 0
+                switch value {
+                case 0:
+                    self.resultLabel.text = ""
+                case 1..<self.randomNumber:
+                    self.resultLabel.text = "Your number '\(value)' is smaller than Unknown"
+                case self.randomNumber + 1..<101:
+                    self.resultLabel.text = "Your number '\(value)' is bigger than Unknown"
+                default:
+                    self.resultLabel.text = "Congrats! You win! it was '\(value)'"
+                    self.cancellables.removeAll()
+                    self.timerCancellables.removeAll()
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func createTimerSubscriber() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            Timer.publish(every: 1.0, on: .main, in: .common)
+                .autoconnect()
+                .scan(0) { counter, _ in
+                    counter + 1
+                }
+                .map { seconds in
+                    self.textTimerLabel.text = "seconds have passed"
+                    return  String(seconds)
+                }
+                .assign(to: \.text, on: self.timerLabel)
+                .store(in: &self.timerCancellables)
+        }
+    }
+    
+    private func setupView() {
+        
+        titlelabel.textAlignment = .center
+        titlelabel.textColor = .systemMint
+        titlelabel.font = UIFont.boldSystemFont(ofSize: 18)
+        titlelabel.text = "Let's try to guess a number from 1 ton 100!"
+        titlelabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(titlelabel)
+        
+        timerLabel.textAlignment = .center
+        timerLabel.textColor = .systemMint
+        timerLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        timerLabel.text = ""
+        titlelabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(timerLabel)
+        
+        textTimerLabel.textAlignment = .center
+        textTimerLabel.textColor = .systemMint
+        textTimerLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        textTimerLabel.text = ""
+        textTimerLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(textTimerLabel)
+        
+        textField.placeholder = "Enter number from 1 to 100"
+        textField.textAlignment = .center
+        textField.backgroundColor = .systemMint.withAlphaComponent(0.8)
+        textField.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        textField.textColor = .systemIndigo
+        textField.layer.cornerRadius = 28
+        textField.clearButtonMode = .whileEditing
+        textField.keyboardType = .numberPad
+        view.addSubview(textField)
+        
+        resultLabel.textAlignment = .center
+        resultLabel.textColor = .systemMint
+        resultLabel.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        resultLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(resultLabel)
+        
+        
+        restartButton.layer.cornerRadius = 24
+        restartButton.layer.borderWidth = 4
+        restartButton.tintColor = .systemIndigo
+        restartButton.setTitle("Start new game", for: .normal)
+        restartButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 27)
+        restartButton.backgroundColor = .systemMint.withAlphaComponent(0.8)
+        restartButton.layer.borderColor = UIColor.systemMint.withAlphaComponent(0.9).cgColor
+        restartButton.layer.shadowColor = UIColor.systemMint.withAlphaComponent(0.9).cgColor
+        restartButton.layer.shadowOpacity = 5
+        restartButton.layer.shadowRadius = 6
+        restartButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        view.addSubview(restartButton)
+        
+        titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            titlelabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 5),
+            titlelabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -5),
+            titlelabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            timerLabel.topAnchor.constraint(equalTo: titlelabel.bottomAnchor, constant: 20),
+            timerLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            timerLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            timerLabel.heightAnchor.constraint(equalToConstant: 60)
+        ])
+        
+        textTimerLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textTimerLabel.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: -10),
+            textTimerLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            textTimerLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            textTimerLabel.heightAnchor.constraint(equalToConstant: 35)
+        ])
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textField.bottomAnchor.constraint(equalTo: resultLabel.topAnchor, constant: -15),
+            textField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            textField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+            textField.heightAnchor.constraint(equalToConstant: 60)
+        ])
+        
+        resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            resultLabel.bottomAnchor.constraint(equalTo: restartButton.topAnchor, constant: -200),
+            resultLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            resultLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            resultLabel.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        restartButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            restartButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            restartButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -70),
+            restartButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            restartButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+            restartButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func createRandomNumber() {
+        randomNumber = Int.random(in: 1...100)
+    }
+    
+    @objc func buttonAction() {
+        timerLabel.text = ""
+        textTimerLabel.text = ""
+        self.timerCancellables.removeAll()
+        self.isCancel = false
+        createRandomNumber()
+        resultLabel.text = ""
+        textField.text = ""
+        createTextFieldSubscriber()
+        createTimerSubscriber()
+    }
+    
+    private func setUpToolBar() {
+        let bar = UIToolbar()
+        let dismissButton = UIBarButtonItem(title: "Dismiss keyboard", style: .plain, target: self, action: #selector(dismissKeyboard))
+        dismissButton.tintColor = .systemIndigo
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        bar.items = [flexSpace, flexSpace, dismissButton]
+        bar.sizeToFit()
+        textField.inputAccessoryView = bar
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newString = NSString(string: textField.text ?? "").replacingCharacters(in: range, with: string)
+        if  let number = Int(newString) {
+            return number >= 1 && number <= 100
+        }
+        return true
+    }
+}
